8820a0f3c726d068d89050b4c0b15445fb414a08
Giampaolo Mancini <giampaolo@trampolineup.com>
2023-03-07 15:45:54 +0100

    Update libraries

--- src/AlPlc_Opta.h
+++ src/AlPlc_Opta.h
@@ -1,11 +1,9 @@
 /*
   This file is part of the Arduino AlPlc_Opta library.
-  Copyright (C) 2021 Arduino AG (http://www.arduino.cc/)
-  SPDX-License-Identifier: MPL-2.0
-  
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+  Copyright (C) 2023 Arduino AG (http://www.arduino.cc/)
+  SPDX-License-Identifier: CC-BY-4.0
+
+  https://creativecommons.org/licenses/by/4.0/
 */
 
 #ifndef ALPLC_OPTA_H
